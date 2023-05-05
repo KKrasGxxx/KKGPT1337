9ae97b76ff09ab32cc180892c723915cbfc0be34
KKrasGxxx <132693594+KKrasGxxx@users.noreply.github.com>
2023-05-06 00:46:17 +0800

    updated

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -237,7 +237,7 @@ export default () => {
             ref={inputRef!}
             disabled={systemRoleEditing()}
             onKeyDown={handleKeydown}
-            placeholder="Enter something..."
+            placeholder="Ask me Anything..."
             autocomplete="off"
             autofocus
             onInput={() => {
